5445d5bd5a3b4bfeba416cd729bb0dccce5bcbd7
Symphony <55611595+lnance6@users.noreply.github.com>
2020-10-29 23:54:00 -0700

    CRUD

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+// This statement sets the execution mode to verbose to produce messages in the terminal regarding the state of the runtime. 
+// This feature can help explain what the application is doing, specifically SQLite.
+const sqlite3 = require('sqlite3').verbose();
+
 //import express
 const express = require('express');
 
@@ -9,6 +13,15 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = new sqlite3.Database('./db/election.db', err => {
+    if (err) {
+      return console.error(err.message);
+    }
+  
+    console.log('Connected to the election database.');
+});
+
 //GET test route 
 // app.get('/', (req, res) => {
 //     res.json({
@@ -16,6 +29,78 @@ app.use(express.json());
 //     });
 // });
 
+// Get all candidates
+// app.get('/api/candidates', (req, res) => {
+//   const sql = `SELECT * FROM candidates`;
+//   const params = [];
+//   db.all(sql, params, (err, rows) => {
+//     if (err) {
+//       res.status(500).json({ error: err.message });
+//       return;
+//     }
+//     res.json({
+//       message: 'success',
+//       data: rows
+//     });
+//   });
+// });
+// Get single candidate
+// app.get('/api/candidate/:id', (req, res) => {
+//   const sql = `SELECT * FROM candidates
+//   WHERE id = ?`;
+//   const params = [req.params.id];
+//   db.get(sql, params, (err, row) => {
+//   if (err) {
+//   res.status(400).json({ error: err.message });
+//   return;
+//   }
+//   res.json({
+//   message: 'success',
+//   data: row
+//   });
+//   });
+//   });
+// Delete a candidate
+// db.run(`DELETE FROM candidates WHERE id = ?`, 1, function(err, result) {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result, this, this.changes);
+// });
+// Delete a candidate
+app.delete('/api/candidate/:id', (req, res) => {
+  const sql = `DELETE FROM candidates WHERE id = ?`;
+  const params = [req.params.id];
+  db.run(sql, params, function(err, result) {
+    if (err) {
+      res.status(400).json({ error: res.message });
+      return;
+    }
+    res.json({
+      message: 'successfully deleted',
+      changes: this.changes
+    });
+  });
+});
+// Create a candidate
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+//   VALUES (?,?,?,?)`;
+//   const params = [1, 'Ronald', 'Firbank', 1];
+//   // ES5 function, not arrow function, to use this
+//   db.run(sql, params, function(err, result) {
+//   if (err) {
+//   console.log(err);
+//   }
+//   console.log(result, this.lastID);
+// });
+
+// GET a single candidate
+// db.get(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+//   if(err) {
+//   console.log(err);
+//   }
+//   console.log(row);
+//   });
 // Route to handle user requests that aren't supported by the app
 // Default response for any other request(Not Found) Catch all
 app.use((req, res) => {
@@ -23,6 +108,9 @@ app.use((req, res) => {
 });
 
 //the function that will start the Express.js server on port 3001
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+// Start server after DB connection
+db.on('open', () => {
+  app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
   });
+});
