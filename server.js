1add0b11822ba4092b3fc5729f504c222424b07a
Symphony <55611595+lnance6@users.noreply.github.com>
2020-10-31 01:05:48 -0700

    added update route

--- server.js
+++ server.js
@@ -59,17 +59,42 @@ app.get('/api/candidate/:id', (req, res) => {
              ON candidates.party_id = parties.id
              WHERE id = ?`;
   const params = [req.params.id];
-  db.get(sql, params, (err, row) => {
-  if (err) {
-  res.status(400).json({ error: err.message });
-  return;
+    db.get(sql, params, (err, row) => {
+      if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: row
+    });
+  });
+});
+
+app.put('/api/candidate/:id', (req, res) => {
+  const errors = inputCheck(req.body, 'party_id');
+
+  if (errors) {
+    res.status(400).json({ error: errors });
+    return;
   }
-  res.json({
-  message: 'success',
-  data: row
+  const sql = `UPDATE candidates SET party_id = ? 
+               WHERE id = ?`;
+  const params = [req.body.party_id, req.params.id];
+
+  db.run(sql, params, function(err, result) {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+
+    res.json({
+      message: 'success',
+      data: req.body,
+      changes: this.changes
+    });
   });
-  });
-  });
+});
 // Delete a candidate
 // db.run(`DELETE FROM candidates WHERE id = ?`, 1, function(err, result) {
 //   if (err) {
@@ -119,6 +144,50 @@ app.post('/api/candidate', ({ body }, res) => {
   });
 });
 
+app.get('/api/parties', (req, res) => {
+  const sql = `SELECT * FROM parties`;
+  const params = [];
+  db.all(sql, params, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+
+    res.json({
+      message: 'success',
+      data: rows
+    });
+  });
+});
+
+app.get('/api/party/:id', (req, res) => {
+  const sql = `SELECT * FROM parties WHERE id = ?`;
+  const params = [req.params.id];
+  db.get(sql, params, (err, row) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+
+    res.json({
+      message: 'success',
+      data: row
+    });
+  });
+});
+
+app.delete('/api/party/:id', (req, res) => {
+  const sql = `DELETE FROM parties WHERE id = ?`;
+  const params = [req.params.id];
+  db.run(sql, params, function(err, result) {
+    if (err) {
+      res.status(400).json({ error: res.message });
+      return;
+    }
+
+    res.json({ message: 'successfully deleted', changes: this.changes });
+  });
+});
 // Create a candidate
 // const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
 //   VALUES (?,?,?,?)`;
