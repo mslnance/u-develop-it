93cc9766598c2fb373a7f63f826cbd45ff7fbf0a
Symphony <55611595+lnance6@users.noreply.github.com>
2020-10-31 00:50:37 -0700

    join to parties table

--- server.js
+++ server.js
@@ -32,36 +32,44 @@ const db = new sqlite3.Database('./db/election.db', err => {
 // });
 
 // Get all candidates
-// app.get('/api/candidates', (req, res) => {
-//   const sql = `SELECT * FROM candidates`;
-//   const params = [];
-//   db.all(sql, params, (err, rows) => {
-//     if (err) {
-//       res.status(500).json({ error: err.message });
-//       return;
-//     }
-//     res.json({
-//       message: 'success',
-//       data: rows
-//     });
-//   });
-// });
+app.get('/api/candidates', (req, res) => {
+  const sql = `SELECT candidates.*, parties.name 
+             AS party_name 
+             FROM candidates 
+             LEFT JOIN parties 
+             ON candidates.party_id = parties.id`;
+  const params = [];
+  db.all(sql, params, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: rows
+    });
+  });
+});
 // Get single candidate
-// app.get('/api/candidate/:id', (req, res) => {
-//   const sql = `SELECT * FROM candidates
-//   WHERE id = ?`;
-//   const params = [req.params.id];
-//   db.get(sql, params, (err, row) => {
-//   if (err) {
-//   res.status(400).json({ error: err.message });
-//   return;
-//   }
-//   res.json({
-//   message: 'success',
-//   data: row
-//   });
-//   });
-//   });
+app.get('/api/candidate/:id', (req, res) => {
+  const sql = `SELECT candidates.*, parties.name 
+             AS party_name 
+             FROM candidates 
+             LEFT JOIN parties 
+             ON candidates.party_id = parties.id
+             WHERE id = ?`;
+  const params = [req.params.id];
+  db.get(sql, params, (err, row) => {
+  if (err) {
+  res.status(400).json({ error: err.message });
+  return;
+  }
+  res.json({
+  message: 'success',
+  data: row
+  });
+  });
+  });
 // Delete a candidate
 // db.run(`DELETE FROM candidates WHERE id = ?`, 1, function(err, result) {
 //   if (err) {
