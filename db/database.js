abf0137d4e2cd2a898fff0982627eff605295a26
Symphony <55611595+lnance6@users.noreply.github.com>
2020-10-31 02:19:07 -0700

    voter crud routes

---
+++ db/database.js
@@ -0,0 +1,13 @@
+// This statement sets the execution mode to verbose to produce messages in the terminal regarding the state of the runtime. 
+// This feature can help explain what the application is doing, specifically SQLite.
+const sqlite3 = require('sqlite3').verbose();
+
+// Connect to database
+const db = new sqlite3.Database('./db/election.db', err => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log('Connected to the election database.');
+});
+
+module.exports = db;
